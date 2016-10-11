060bac270fddc9cab88f6c9fb877d753fdab38d0
Patrick Geddie <geddiep@gmail.com>
2016-10-10 21:30:17 -0400

    added front end form to test submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,5 +8,20 @@ socket.on('message',function(message){
 
 	console.log('New message:');
 	console.log(message.text);
-	
-})
+
+});
+
+//handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event){
+	event.preventDefault();
+	$message = $form.find('input[name=message]');
+	socket.emit('message',{
+		text: $message.val()
+	});
+
+	//remove message
+	$message.val('');
+
+});
