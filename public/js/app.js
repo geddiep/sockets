feac769106401c5310a0f6113c2d86e8132a7237
Patrick Geddie <geddiep@gmail.com>
2016-10-11 21:09:20 -0400

    add message to browser

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,8 @@ socket.on('message',function(message){
 	console.log('New message:');
 	console.log(message.text);
 
+	jQuery('.messages').append('<p>' + message.text + '</p>');
+
 });
 
 //handles submitting of new message
